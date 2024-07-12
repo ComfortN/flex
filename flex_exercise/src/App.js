556b392160ce51d2cdfb7b7a3e0e9973a7ea509a
Comfort Ngwenya <>
2024-07-12 06:57:17 +0200

    flex

---
+++ flex_exercise/src/App.js
@@ -0,0 +1,59 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='navbar'>
+
+        <div className='circle'>
+          <div className='circles'></div>
+        </div>
+
+        <div className='shapes'>
+          <div className='rectangle'></div>
+          <div className='square'></div>
+          <div className='rectangle'></div>
+          <div className='circles'></div>
+        </div>
+        
+      </div>
+      <div className='rectangl'></div>
+
+
+      <div className='main'>
+        <div className='squa'>
+          <div className='bigsquare'></div>
+      </div>
+
+        <div className='rectangls'>
+          <div className='rect'></div>
+          <div className='rect'></div>
+          <div className='rect'></div>
+          <div className='rect'></div>
+        </div>
+      </div>
+
+      <div className='rectangl'></div>
+      
+
+
+      <div className='rectang'>
+        <div className='squar'></div>
+        <div className='round'></div>
+        <div className='squar'></div>
+      </div>
+
+      <div className='footer'>
+      <div className='foot'></div>
+      <div className='foot'></div>
+      </div>
+      
+
+
+
+    </div>
+  );
+}
+
+export default App;
